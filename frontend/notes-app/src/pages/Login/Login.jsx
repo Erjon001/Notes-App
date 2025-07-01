3cd1131c7ae151b288ec802dc42f38f058a7c3a8
Erjon Rrapi <erjonrrapi429@gmail.com>
2025-07-01 17:58:26 +0200

    feat: update routing and enhance login page with form and navigation

--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -1,9 +1,30 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import { Link } from "react-router-dom";
 const Login = () => {
   return (
     <>
       <Navbar />
+      <div className="flex item-center justify-center mt-28">
+        <div className="w-96 border rounded bg-white px-7 py-10 ">
+          <form onSubmit={() => {}}>
+            <h4 className="text-2xl mb-7">Login</h4>
+            <input type="text" placeholder="Email" className="input-box" />
+            <button type="submit" className="btn-primary">
+              Login
+            </button>
+            <p className="text-sm text-center mt-4 ">
+              Not registered yet?{" "}
+              <Link
+                to="/signUp"
+                className="font-medium text-(--color-primary) underline">
+                {" "}
+                Create an Accounts
+              </Link>
+            </p>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
