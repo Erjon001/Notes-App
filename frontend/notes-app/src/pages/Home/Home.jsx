01b3b6b377af980ce58a42a953fbddd0d0ddcd6d
Erjon Rrapi <erjonrrapi429@gmail.com>
2025-07-11 18:04:48 +0200

    feat: add ProfileInfo component and integrate it into Navbar

--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-
-function Home() {
-  return <div>Home</div>;
-}
+import Navbar from "../../components/Navbar/Navbar";
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
 
 export default Home;
