84e218634cf01d938239d43e99d0e22d177b479f
Erjon Rrapi <erjonrrapi429@gmail.com>
2025-07-24 21:05:33 +0200

    feat: enhance AddEditNotes component with tag management and error handling

--- frontend/notes-app/src/components/Input/TagInput.jsx
+++ frontend/notes-app/src/components/Input/TagInput.jsx
@@ -20,14 +20,23 @@ const TagInput = ({ tags, setTags }) => {
     }
   };
 
+  const handleRemoveTag = (tagToRemove) => {
+    setTags(tags.filter((tag) => tag !== tagToRemove));
+  };
+
   return (
     <div>
       {tags?.length > 0 && (
-        <div className="">
+        <div className="flex items-center flex-wrap mt-2 gap-2">
           {tags.map((tag, index) => (
-            <span key={index} className="">
+            <span
+              key={index}
+              className="flex items-center gap-2 text-sm text-slate-900 bg-slate-100 rounded py-1 px-3">
               # {tag}
-              <button onClick={() => {}}>
+              <button
+                onClick={() => {
+                  handleRemoveTag(tag);
+                }}>
                 <MdClose />
               </button>
             </span>
@@ -35,10 +44,11 @@ const TagInput = ({ tags, setTags }) => {
         </div>
       )}
 
-      <div className="flex items-center gap-4 mt-3">
+      <div className="flex items-center gap-4 mt-3 ">
         <input
           className="text-sm bg-transparent border px-3 py-2 rounded outline-none"
           type="text"
+          value={inputValue}
           placeholder="Add Tags"
           onChange={handleInputChange}
           onKeyDown={handleKeyDown}
--- frontend/notes-app/src/pages/Home/AddEditNotes.jsx
+++ frontend/notes-app/src/pages/Home/AddEditNotes.jsx
@@ -1,15 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import TagInput from "../../components/Input/TagInput";
+import { MdClose } from "react-icons/md";
 
-const AddEditNotes = () => {
+const AddEditNotes = ({ noteData, type, onClose }) => {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [tags, setTags] = useState([]);
+
+  const [error, setError] = useState(null);
+
+  const addNewNote = async () => {};
+  const editNote = async () => {};
+
+  const handleAddNote = () => {
+    if (!title) {
+      setError("Please enter a title!");
+      return;
+    }
+
+    if (!content) {
+      setError("Please enter the content!");
+      return;
+    }
+
+    setError("");
+
+    if (type === "edit") {
+      editNote();
+    } else {
+      addNewNote();
+    }
+  };
   return (
-    <div>
+    <div className="relative">
+      <button
+        className="w-10 h-10 rounded-full flex items-center justify-center absolute -right-3 -top-3 hover:bg-slate-50 "
+        on
+        onClick={onClose}>
+        <MdClose className="text-xl text-slate-400" />
+      </button>
       <div className="flex flex-col gap-2">
         <label className="input-label">TITLE</label>
         <input
           type="text"
           className="text-2xl text-slate-950 outline-none"
           placeholder="Go to the gym at 5"
+          value={title}
+          onChange={({ target }) => setTitle(target.value)}
         />
       </div>
 
@@ -20,15 +57,19 @@ const AddEditNotes = () => {
           className="text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded"
           rows={10}
           placeholder="Content"
+          value={content}
+          onChange={({ target }) => setContent(target.value)}
         />
       </div>
 
       <div className="mt-3">
         <label className="input-label">TAGS</label>
-        <TagInput></TagInput>
+        <TagInput tags={tags} setTags={setTags} />
       </div>
-
-      <button className="btn-primary font-medium mt-5 p-3" onClick={() => {}}>
+      {error && <p className="text-red-500 text-xs p-3">{error}</p>}
+      <button
+        className="btn-primary font-medium mt-3 p-3"
+        onClick={handleAddNote}>
         ADD
       </button>
     </div>
