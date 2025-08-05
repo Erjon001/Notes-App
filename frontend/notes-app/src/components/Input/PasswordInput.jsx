75fac96017b948b8574bf11d0bb42dbc15af8950
Erjon Rrapi <erjonrrapi429@gmail.com>
2025-08-05 19:12:14 +0200

    fix: correct spacing in PasswordInput component's wrapper div

--- frontend/notes-app/src/components/Input/PasswordInput.jsx
+++ frontend/notes-app/src/components/Input/PasswordInput.jsx
@@ -8,7 +8,7 @@ const PasswordInput = ({ value, onChange, placeholder }) => {
     setIsShowPassword(!isShowPassword);
   };
   return (
-    <div className="flex items-center bg-transparent border-[1.5px] px-5 rounded mb-3">
+    <div className="flex items-center bg-transparent border-[1.5px] px-5 rounded mb-3 ">
       <input
         value={value}
         onChange={onChange}
