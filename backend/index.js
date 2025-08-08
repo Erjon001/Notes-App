504bc25013e02b6aeec77300370466e140c02db6
Erjon Rrapi <erjonrrapi429@gmail.com>
2025-08-08 21:24:15 +0200

    feat: implement user registration endpoint with validation and token generation

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ const mongoose = require("mongoose");
 
 mongoose.connect(config.connectionString);
 
+const User = require("./models/user.model");
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -24,7 +26,52 @@ app.get("/", (req, res) => {
   res.json({ data: "hello" });
 });
 
-app.post("/create_account", async (req, res) => {});
+app.post("/create-account", async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  if (!fullName) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Full name is required" });
+  }
+
+  if (!email) {
+    return res.status(400).json({ error: true, message: "Email is required" });
+  }
+
+  if (!password) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Password is required" });
+  }
+
+  const isUser = await User.findOne({ email: email });
+
+  if (isUser) {
+    return res
+      .status(400)
+      .json({ error: true, message: "User already exists" });
+  }
+
+  const user = new User({
+    fullName,
+    email,
+    password,
+  });
+
+  await user.save();
+
+  const accessToken = jwt.sign({ user }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "30000m",
+  });
+
+  return res.json({
+    error: false,
+    user,
+    accessToken,
+    message: "Registration successful",
+  });
+});
 
 app.listen(8000);
 
