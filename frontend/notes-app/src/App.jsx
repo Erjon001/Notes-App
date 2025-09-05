55c4613ff3d90ee36bb56ebad328a0abcac037a9
Erjon Rrapi <erjonrrapi429@gmail.com>
2025-09-05 19:10:14 +0200

    feat: add missing route for home page in App component

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const routes = (
   <Router>
     <Routes>
+      <Route path="/" exact element={<Home />} />
       <Route path="/dashboard" exact element={<Home />} />
       <Route path="/login" exact element={<Login />} />
       <Route path="/signUp" exact element={<SignUp />} />
