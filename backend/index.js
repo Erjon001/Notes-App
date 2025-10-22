00d92c84a908962c3619cc513a0e99c40a6f581d
Erjon Rrapi <erjonrrapi429@gmail.com>
2025-10-22 19:08:56 +0200

    fix: simplify isPinned assignment logic in update-note-pinned API

--- backend/index.js
+++ backend/index.js
@@ -267,7 +267,7 @@ app.put("/update-note-pinned/:noteId", authenticateToken, async (req, res) => {
       return res.status(404).json({ error: true, message: "Note not found " });
     }
 
-    if (isPinned) note.isPinned = isPinned || false;
+    note.isPinned = isPinned;
 
     await note.save();
 
