a6b8173157b37be8d2cb208ddddbfd4218555184
Erjon Rrapi <erjonrrapi429@gmail.com>
2025-11-07 23:30:30 +0100

    feat: implement conditional navigation based on signup status

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -2,14 +2,28 @@ import React from "react";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+const isSignedUp = !!localStorage.getItem("accessToken");
 
 const routes = (
   <Router>
     <Routes>
-      <Route path="/dashboard" exact element={<Home />} />
-      <Route path="/login" exact element={<Login />} />
-      <Route path="/signUp" exact element={<SignUp />} />
+      <Route
+        path="/"
+        element={
+          isSignedUp ? <Navigate to="/dashboard" /> : <Navigate to="/login" />
+        }
+      />
+
+      <Route path="/dashboard" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signUp" element={<SignUp />} />
     </Routes>
   </Router>
 );
